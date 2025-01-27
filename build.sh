df8a057ff0dc9b108bccc6006abdd6c1d259cb09
astrovm <~@4st.li>
2025-01-27 00:52:23 -0300

    chore: Add scrcpy repository and package to build script

--- build.sh
+++ build.sh
@@ -5,12 +5,14 @@ set -ouex pipefail
 # Add repositories
 dnf5 config-manager addrepo --from-repofile https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
 dnf5 config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 copr enable -y zeno/scrcpy
 
 # Install packages
 dnf5 install -y \
     code \
     neovim \
     android-tools \
+    scrcpy \
     docker-ce \
     docker-ce-cli \
     containerd.io \
