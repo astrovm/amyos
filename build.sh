2a80975bba4245750aa4e388f4c92fb5d5d39751
astrovm <~@4st.li>
2025-01-27 00:10:53 -0300

    chore: Create /var/opt directory for build script

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+# Create directories
+mkdir -p /var/opt
+
 # Add repositories
 dnf5 config-manager addrepo --from-repofile https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 dnf5 config-manager addrepo --from-repofile https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
