8ac26046dcb3fe11fd9fad5ddcfd57e9b5270d1d
astrovm <~@4st.li>
2025-01-26 21:24:35 -0300

    chore: Update repository management to use dnf5

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Add Brave Browser Repository
-dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 config-manager addrepo --from-repofile https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 # Add VSCode Repository
-dnf config-manager --add-repo https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
+dnf5 config-manager addrepo --from-repofile https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
 
 # Install apps
 dnf install -y brave-browser code
