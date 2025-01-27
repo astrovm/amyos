e02c927f0083c5342f5eeaf7edb1ecd7ae603176
astrovm <~@4st.li>
2025-01-27 16:27:00 -0300

    chore: Update VSCode repository URL in build script
    - Replace gist-based VSCode repository URL with GitHub-hosted URL
    - Maintain consistent repository configuration method
    - Ensure reliable access to VSCode repository configuration

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 mkdir -p /var/opt
 
 # Add repositories
-dnf5 config-manager addrepo --from-repofile https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
+dnf5 config-manager addrepo --from-repofile https://github.com/astrovm/amyos/raw/refs/heads/main/repo_files/vscode.repo
 dnf5 config-manager addrepo --from-repofile https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 dnf5 config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 config-manager addrepo --from-repofile https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
