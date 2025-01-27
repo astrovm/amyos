8fdd047c4a22f78144de62128a7b30aa2b472c28
astrovm <~@4st.li>
2025-01-26 22:07:12 -0300

    chore: Consolidate package installation and repository management
    - Add repositories for Brave, VSCode, and Docker in a single command
    - Install additional packages: Android Tools and Neovim
    - Streamline package installation process
    - Enable Docker service

--- build.sh
+++ build.sh
@@ -2,28 +2,26 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# Create necessary directories
+# Create directories
 mkdir -p /var/opt
 
-# Install Brave Browser
-dnf5 config-manager addrepo --from-repofile https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf5 install -y brave-browser
+# Add repositories
+dnf5 config-manager addrepo --from-repofile \
+    https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo \
+    https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
 
-# Install VSCode
-dnf5 config-manager addrepo --from-repofile https://gist.github.com/astrovm/fb1a053d82c7c745fdb46c674b8c0002/raw/b93e2ef423a6432702a05566131f4c5431216be6/vscode.repo
-dnf5 install -y code
+# Install packages
+dnf5 install -y \
+    code \
+    neovim \
+    android-tools \
+    brave-browser \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
 
-# Install Docker
-dnf5 config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
-dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Enable services
 systemctl enable docker
-
-# Install Neovim
-dnf5 install -y neovim
