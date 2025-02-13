76138f99b77bf2762e96638aa5cf430a038eaaf1
astrovm <~@4st.li>
2025-02-13 01:41:55 -0300

    Remove Gamescope Session

--- build_files/build.sh
+++ build_files/build.sh
@@ -60,12 +60,6 @@ for repo in "atim/ubuntu-fonts:ubuntu-family-fonts" \
   dnf5 -y copr disable "$repo_name"
 done
 
-# Install Gamescope Session
-log "Installing Gamescope Session"
-mkdir -p /usr/share/gamescope-session-plus
-curl --retry 3 -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
-dnf5 -y install --repo copr:copr.fedorainfracloud.org:kylegospo:bazzite gamescope-session-plus gamescope-session-steam
-
 # Install Cursor
 log "Installing Cursor"
 # GUI version
