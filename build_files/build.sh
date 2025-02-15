db28961966782a7356900f1778065f9b3496eff6
astrovm <~@4st.li>
2025-02-15 16:44:45 -0300

    Refactor Cursor installation process in build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -79,7 +79,6 @@ declare -A RPM_PACKAGES=(
 
 log "Starting Amy OS build process"
 
-# Install RPM packages
 log "Installing RPM packages"
 for repo in "${!RPM_PACKAGES[@]}"; do
   read -ra pkg_array <<<"${RPM_PACKAGES[$repo]}"
@@ -96,37 +95,35 @@ for repo in "${!RPM_PACKAGES[@]}"; do
   fi
 done
 
-# Install Cursor
-log "Installing Cursor"
-# GUI version
-mkdir -p /tmp/cursor-gui
-curl --retry 3 -Lo /tmp/cursor-gui/cursor.appimage "https://downloader.cursor.sh/linux/appImage/x64"
-chmod +x /tmp/cursor-gui/cursor.appimage
-cd /tmp/cursor-gui && ./cursor.appimage --appimage-extract
-chmod -R a+rX /tmp/cursor-gui/squashfs-root
-cp -r /tmp/cursor-gui/squashfs-root/usr/share/icons/hicolor/* /usr/share/icons/hicolor
-rm -r /tmp/cursor-gui/squashfs-root/usr/share/icons/hicolor
+log "Installing Cursor GUI"
+GUI_DIR="/tmp/cursor-gui"
+mkdir -p "$GUI_DIR"
+curl --retry 3 -Lo "$GUI_DIR/cursor.appimage" "https://downloader.cursor.sh/linux/appImage/x64"
+chmod +x "$GUI_DIR/cursor.appimage"
+(cd "$GUI_DIR" && ./cursor.appimage --appimage-extract)
+chmod -R a+rX "$GUI_DIR/squashfs-root"
+cp -r "$GUI_DIR/squashfs-root/usr/share/icons/hicolor/"* /usr/share/icons/hicolor
+rm -r "$GUI_DIR/squashfs-root/usr/share/icons/hicolor"
 mkdir -p /usr/share/cursor/bin
-mv /tmp/cursor-gui/squashfs-root/* /usr/share/cursor
+mv "$GUI_DIR/squashfs-root/"* /usr/share/cursor
 install -m 0755 /usr/share/cursor/resources/app/bin/cursor /usr/share/cursor/bin/cursor
 ln -s /usr/share/cursor/bin/cursor /usr/bin/cursor
-# CLI version
-mkdir -p /tmp/cursor-cli
-curl --retry 3 -Lo /tmp/cursor-cli/cursor-cli.tar.gz "https://api2.cursor.sh/updates/download-latest?os=cli-alpine-x64"
-tar -xzf /tmp/cursor-cli/cursor-cli.tar.gz -C /tmp/cursor-cli
-install -m 0755 /tmp/cursor-cli/cursor /usr/share/cursor/bin/cursor-tunnel
+
+log "Installing Cursor CLI"
+CLI_DIR="/tmp/cursor-cli"
+mkdir -p "$CLI_DIR"
+curl --retry 3 -Lo "$CLI_DIR/cursor-cli.tar.gz" "https://api2.cursor.sh/updates/download-latest?os=cli-alpine-x64"
+tar -xzf "$CLI_DIR/cursor-cli.tar.gz" -C "$CLI_DIR"
+install -m 0755 "$CLI_DIR/cursor" /usr/share/cursor/bin/cursor-tunnel
 ln -s /usr/share/cursor/bin/cursor-tunnel /usr/bin/cursor-cli
 
-# Enable services
 log "Enabling system services"
 systemctl enable docker libvirtd
 
-# Disable autostart
 log "Disabling autostart"
 rm /etc/xdg/autostart/{solaar.desktop,com.cloudflare.WarpTaskbar.desktop}
 rm /etc/skel/.config/autostart/steam.desktop
 
-# Configure system
 log "Configuring system"
 echo "import \"/usr/share/amyos/just/install-apps.just\"" >>/usr/share/ublue-os/justfile
 {
