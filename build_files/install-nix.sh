18f7a745037e8c7c30ed0e66fd372d64ecdf16df
astrovm <~@4st.li>
2025-02-20 16:25:06 -0300

    Add systemd service symlinks for Nix daemon

--- build_files/install-nix.sh
+++ build_files/install-nix.sh
@@ -12,6 +12,9 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 tar --zstd -cvf /usr/share/nix.tar.zst /nix
 
 log "Enabling system services"
+mkdir -p /usr/lib/systemd/system
+ln -s /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.service /usr/lib/systemd/system/
+ln -s /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.socket /usr/lib/systemd/system/
 systemctl enable nix-setup.service nix-directory.service nix.mount ensure-symlinked-units-resolve.service nix-daemon.socket
 
 log "Nix install completed"
