92e74cb2d52de9d2352f20f77304f0f85fc14512
astrovm <~@4st.li>
2025-02-20 16:48:14 -0300

    Add Nix users to system passwd

--- build_files/install-nix.sh
+++ build_files/install-nix.sh
@@ -11,10 +11,13 @@ log "Installing Nix"
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-start-daemon --no-confirm
 tar --zstd -cvf /usr/share/nix.tar.zst /nix
 
+log "Copying Nix users to /usr/lib/passwd"
+grep "^nixbld" /etc/passwd >> /usr/lib/passwd
+
 log "Enabling system services"
 mkdir -p /usr/lib/systemd/system
 ln -s /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.service /usr/lib/systemd/system/
 ln -s /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.socket /usr/lib/systemd/system/
-systemctl enable nix-setup.service nix-directory.service nix.mount ensure-symlinked-units-resolve.service nix-daemon.socket
+systemctl enable nix-setup.service nix-directory.service nix.mount ensure-symlinked-units-resolve.service
 
 log "Nix install completed"
