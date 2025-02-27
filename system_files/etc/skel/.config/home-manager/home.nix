ae98dd0da29ec434e234d57bc4dd24275c4beeb0
astrovm <~@4st.li>
2025-02-27 02:36:56 -0300

    Improve Zsh keybindings formatting and comments
    - Align keybindings for better readability
    - Update comments to use consistent capitalization
    - Clarify navigation key descriptions
    - Maintain existing keybinding functionality

--- system_files/etc/skel/.config/home-manager/home.nix
+++ system_files/etc/skel/.config/home-manager/home.nix
@@ -51,29 +51,29 @@
       initExtra = ''
         ## Keybindings section
         bindkey -e
-        bindkey '^[[H'  beginning-of-line                               # Home key (xterm)
-        bindkey '^[[OH' beginning-of-line                               # Home key (smkx mode)
-        bindkey '^[[1~' beginning-of-line                               # Home key (screen & tmux)
-        bindkey '^[[7~' beginning-of-line                               # Home key (urxvt)
-        bindkey '^[[F'  end-of-line                                     # End key (xterm)
-        bindkey '^[[OF' end-of-line                                     # End key (smkx mode)
-        bindkey '^[[4~' end-of-line                                     # End key (screen & tmux)
-        bindkey '^[[8~' end-of-line                                     # End key (urxvt)
-        bindkey '^[[2~' overwrite-mode                                  # Insert key
-        bindkey '^[[3~' delete-char                                     # Delete key
-        bindkey '^[[C'  forward-char                                    # Right key
-        bindkey '^[[D'  backward-char                                   # Left key
-        bindkey '^[[5~' history-beginning-search-backward               # Page up key
-        bindkey '^[[6~' history-beginning-search-forward                # Page down key
-        bindkey '^[[A'  up-line-or-history                              # Up key
-        bindkey '^[[B'  down-line-or-history                            # Down key
-        # Navigate words with ctrl+arrow keys
-        bindkey '^[Oc' forward-word                                     #
-        bindkey '^[Od' backward-word                                    #
-        bindkey '^[[1;5D' backward-word                                 #
-        bindkey '^[[1;5C' forward-word                                  #
-        bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
-        bindkey '^[[Z' undo                                             # Shift+tab undo last action
+        bindkey '^[[H'  beginning-of-line                  # Home key (xterm)
+        bindkey '^[[OH' beginning-of-line                  # Home key (smkx mode)
+        bindkey '^[[1~' beginning-of-line                  # Home key (screen & tmux)
+        bindkey '^[[7~' beginning-of-line                  # Home key (urxvt)
+        bindkey '^[[F'  end-of-line                        # End key (xterm)
+        bindkey '^[[OF' end-of-line                        # End key (smkx mode)
+        bindkey '^[[4~' end-of-line                        # End key (screen & tmux)
+        bindkey '^[[8~' end-of-line                        # End key (urxvt)
+        bindkey '^[[2~' overwrite-mode                     # Insert key
+        bindkey '^[[3~' delete-char                        # Delete key
+        bindkey '^[[C'  forward-char                       # Right key
+        bindkey '^[[D'  backward-char                      # Left key
+        bindkey '^[[5~' history-beginning-search-backward  # Page up key
+        bindkey '^[[6~' history-beginning-search-forward   # Page down key
+        bindkey '^[[A'  up-line-or-history                 # Up key
+        bindkey '^[[B'  down-line-or-history               # Down key
+        # Navigate words with Ctrl+Arrow keys
+        bindkey '^[Oc' forward-word                        #
+        bindkey '^[Od' backward-word                       #
+        bindkey '^[[1;5D' backward-word                    #
+        bindkey '^[[1;5C' forward-word                     #
+        bindkey '^H' backward-kill-word                    # Delete previous word with Ctrl+Backspace
+        bindkey '^[[Z' undo                                # Shift+Tab undo last action
       '';
     };
 
